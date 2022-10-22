bef3b3d3dbe872c37c0bb7c512f7c5bf1310bc47
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-10-22 19:13:21 +0200

    fix: minor refactoring

--- src/restoration/filters/BaseFilter.py
+++ src/restoration/filters/BaseFilter.py
@@ -14,18 +14,25 @@ class BaseFilter:
         self.img = img
         self.window_len = window_len
 
-    def get_pixels_in_window(self, row_param, col_param):
+    def get_pixels_in_window(self, _row, _col):
         img_shape = get_shape.get_shape(self.img)
         img_height, img_width = img_shape[get_shape.HEIGHT], img_shape[get_shape.WIDTH]
-        offset = int(self.window_len/2)
-        min_row, min_col = row_param - offset, col_param - offset
-        max_row, max_col = row_param + offset, col_param + offset
-        pixels = []
-        for row in range(min_row, max_row + 1):
-            row = self.get_mirrored_index(img_height, row)
-            new_row = [self.img[row][self.get_mirrored_index(img_width, pix)] for pix in range(min_col, max_col + 1)]
-            pixels.append(new_row)
+        offset = int(self.window_len / 2)
+        min_row, min_col, max_row, max_col = self.get_window_limits(_row, _col, offset)
+        pixels = [
+            [self.img[self.get_mirrored_index(img_height, row)][self.get_mirrored_index(img_width, pix)]
+             for pix in range(min_col, max_col + 1)]
+            for row in range(min_row, max_row + 1)
+        ]
         return list(chain.from_iterable(pixels))
+
+    @staticmethod
+    def get_window_limits(_row, _col, offset):
+        """
+        Returns:
+             min_row, min_col, max_row, max_col
+        """
+        return _row - offset, _col - offset, _row + offset, _col + offset
 
     def get_mirrored_index(self, img_dim, ind):
         return ind + self.window_len - 1 if ind < 0 else (ind - self.window_len - 1 if ind >= img_dim else ind)
@@ -35,7 +42,6 @@ class BaseFilter:
         return np.mean(pixels)
 
     def get_filtered_img(self):
-        img_rows = enumerate(self.img)
         img = [[self.get_corrected_pixel_value(row_ind, pix_ind) for pix_ind, pix_val in enumerate(self.img[row_ind])]
-               for row_ind, row_val in img_rows]
+               for row_ind, row_val in enumerate(self.img)]
         return np.array(img, dtype=np.uint8)
